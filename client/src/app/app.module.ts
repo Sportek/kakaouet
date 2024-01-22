a5a742527176d621a3aaeed9ef0faa0dc7d12fc5
Gabriel Landry <gablandry31@gmail.com>
2024-01-22 18:03:19 +0000

    Feature/footer header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
-import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { BackgroundComponent } from '@app/components/background/background.component';
+import { GlobalLayoutComponent } from '@app/components/global-layout/global-layout.component';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { AppComponent } from '@app/pages/app/app.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { BackgroundComponent } from './background/background.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { AdminLoginPageComponent } from './pages/admin/admin-login-page/admin-login-page.component';
 import { AdminPageComponent } from './pages/admin/admin-page/admin-page.component';
 import { CreatePageComponent } from './pages/create-page/create-page.component';
@@ -26,13 +27,14 @@ import { JoinPageComponent } from './pages/join-page/join-page.component';
     declarations: [
         AppComponent,
         MainPageComponent,
-        PlayAreaComponent,
-        SidebarComponent,
         BackgroundComponent,
         CreatePageComponent,
         AdminPageComponent,
         JoinPageComponent,
         AdminLoginPageComponent,
+        GlobalLayoutComponent,
+        FooterComponent,
+        HeaderComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, MatButtonModule],
     providers: [],
