4f3e1d039e4e877bb0e29850e518acc49a2e2d95
Gabriel Landry <gablandry31@gmail.com>
2024-02-05 16:49:45 +0000

    Fix/authentification

--- client/src/app/pages/admin/admin-login-page/admin-login-page.component.ts
+++ client/src/app/pages/admin/admin-login-page/admin-login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AdminLoginState, UserService } from '@app/services/user/user.service';
 
@@ -7,7 +7,7 @@ import { AdminLoginState, UserService } from '@app/services/user/user.service';
     templateUrl: './admin-login-page.component.html',
     styleUrls: ['./admin-login-page.component.scss'],
 })
-export class AdminLoginPageComponent {
+export class AdminLoginPageComponent implements OnInit {
     password: string;
     loggedState: AdminLoginState;
 
@@ -18,6 +18,13 @@ export class AdminLoginPageComponent {
         this.userService = userService;
         this.router = router;
     }
+
+    ngOnInit(): void {
+        this.userService.loggedState.subscribe((state) => {
+            if (state === AdminLoginState.LoggedIn) this.router.navigateByUrl('/admin', { replaceUrl: true });
+        });
+    }
+
     submitPassword() {
         this.userService.login(this.router, this.password);
     }
