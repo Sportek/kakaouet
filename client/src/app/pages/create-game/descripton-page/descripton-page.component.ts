e04666880c814c327e9a165cffdc371f1905b404
Siiryas <lexois22@gmail.com>
2024-02-13 01:10:12 +0000

    Feature/notification testing jeu

--- client/src/app/pages/create-game/descripton-page/descripton-page.component.ts
+++ client/src/app/pages/create-game/descripton-page/descripton-page.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core'; // chat gpt Change
-import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 // import { Question } from '@app/services/quiz/question';
 import { QuizService } from '@app/services/quiz/quiz.service';
+import { Variables } from '@common/enum-variables';
 import { Question, Quiz } from '@common/types';
 
 @Component({
@@ -12,15 +14,20 @@ import { Question, Quiz } from '@common/types';
 export class DescriptonPageComponent implements OnInit {
     game: Quiz;
     question: Question[];
+    notFound: number;
 
+    // eslint-disable-next-line max-params
     constructor(
         private quizService: QuizService,
         private route: ActivatedRoute,
+        private router: Router,
+        private snackBar: MatSnackBar,
         private cd: ChangeDetectorRef, // injecter ChangerDetectorRef
     ) {}
 
     ngOnInit() {
         const routeParams = this.route.snapshot.paramMap;
+        this.notFound = Variables.NotFound;
         const gameIdFromRoute = routeParams.get('gameId');
         if (gameIdFromRoute) {
             this.getQuiz(gameIdFromRoute);
@@ -33,6 +40,42 @@ export class DescriptonPageComponent implements OnInit {
                 this.game = quiz;
                 this.question = quiz.questions;
                 this.cd.detectChanges();
+            },
+        });
+    }
+    testGame(gameId: string) {
+        this.checkQuizBeforeNavigation(gameId, '/testing');
+    }
+
+    createGame(gameId: string) {
+        this.checkQuizBeforeNavigation(gameId, '/create', false);
+    }
+
+    checkQuizBeforeNavigation(gameId: string, path: string, includeId: boolean = true) {
+        this.quizService.getQuizById(gameId).subscribe({
+            next: (quiz) => {
+                if (quiz.visibility === false) {
+                    this.snackBar.open('Ce jeu est actuellement invisible.', 'Fermer', {
+                        duration: 5000,
+                    });
+                    this.router.navigate(['/create']);
+                } else if (includeId) {
+                    this.router.navigate([path, gameId]);
+                } else {
+                    this.router.navigate([path]);
+                }
+            },
+            error: (error) => {
+                if (error.status === this.notFound) {
+                    this.snackBar.open('Ce jeu a été supprimé, veuillez sélectionner un autre jeu', 'Fermer', {
+                        duration: 5000,
+                    });
+                    this.router.navigate(['/create']);
+                } else {
+                    this.snackBar.open('Une erreur est survenue. Veuillez réessayer.', 'Fermer', {
+                        duration: 5000,
+                    });
+                }
             },
         });
     }
