1541382085b1cb4c268db0f7a00f4973777803ef
Gabriel Landry <gablandry31@gmail.com>
2024-02-14 03:41:37 +0000

    Mise en place du projet terminé, sprint 1

--- server/app/services/question/mock-question.ts
+++ server/app/services/question/mock-question.ts
@@ -3,32 +3,32 @@ import { QuestionDto } from '@app/model/dto/question/question.dto';
 export const mockQuestions: QuestionDto[] = [
     {
         type: 'QCM',
-        label: 'Who is the author of "The Lord of the Rings"?',
+        label: 'Qui est l\'auteur du "Seigneur des Anneaux" ?',
         points: 10,
         choices: [
-            { label: 'J.K. Rowling', isCorrect: true },
-            { label: 'J.R.R. Tolkien', isCorrect: false },
+            { label: 'J.K. Rowling', isCorrect: false },
+            { label: 'J.R.R. Tolkien', isCorrect: true },
             { label: 'George R.R. Martin', isCorrect: false },
             { label: 'C.S. Lewis', isCorrect: false },
         ],
     },
     {
         type: 'QCM',
-        label: 'What is the name of the hobbit who inherits the One Ring from Bilbo?',
+        label: "Quel est le nom du hobbit qui hérite de l'Anneau Unique de Bilbon ?",
         points: 15,
         choices: [
-            { label: 'Meriadoc Brandybuck', isCorrect: false },
-            { label: 'Peregrin Took', isCorrect: false },
-            { label: 'Frodo Baggins', isCorrect: true },
-            { label: 'Samwise Gamgee', isCorrect: false },
+            { label: 'Meriadoc Brandebouc', isCorrect: false },
+            { label: 'Peregrin Touc', isCorrect: false },
+            { label: 'Frodon Sacquet', isCorrect: true },
+            { label: 'Samsagace Gamegie', isCorrect: false },
         ],
     },
     {
         type: 'QCM',
-        label: 'Which city is the capital of Gondor?',
+        label: 'Quelle ville est la capitale du Gondor ?',
         points: 10,
         choices: [
-            { label: 'Rivendell', isCorrect: false },
+            { label: 'Fondcombe', isCorrect: false },
             { label: 'Minas Tirith', isCorrect: true },
             { label: 'Osgiliath', isCorrect: false },
             { label: 'Edoras', isCorrect: false },
@@ -36,18 +36,18 @@ export const mockQuestions: QuestionDto[] = [
     },
     {
         type: 'QCM',
-        label: 'Who is the steward of Gondor before Aragorn becomes king?',
+        label: "Qui est l'intendant du Gondor avant qu'Aragorn ne devienne roi ?",
         points: 15,
         choices: [
             { label: 'Denethor', isCorrect: true },
             { label: 'Boromir', isCorrect: false },
             { label: 'Faramir', isCorrect: false },
-            { label: 'Theoden', isCorrect: false },
+            { label: 'Théoden', isCorrect: false },
         ],
     },
     {
         type: 'QCM',
-        label: 'What is the elvish word for friend, used to open the Doors of Durin?',
+        label: 'Quel est le mot elfique pour ami, utilisé pour ouvrir les Portes de Durin ?',
         points: 10,
         choices: [
             { label: 'Mellon', isCorrect: true },
@@ -58,7 +58,7 @@ export const mockQuestions: QuestionDto[] = [
     },
     {
         type: 'QCM',
-        label: 'Which character said, "Not all those who wander are lost"?',
+        label: 'Quel personnage a dit : "Tous ceux qui errent ne sont pas perdus" ?',
         points: 15,
         choices: [
             { label: 'Gandalf', isCorrect: false },
@@ -69,32 +69,32 @@ export const mockQuestions: QuestionDto[] = [
     },
     {
         type: 'QCM',
-        label: 'How many Rings of Power were forged in the Second Age?',
+        label: "Combien d'Anneaux de Pouvoir ont été forgés au Deuxième Âge ?",
         points: 10,
         choices: [
-            { label: 'One', isCorrect: false },
-            { label: 'Nine', isCorrect: false },
-            { label: 'Twenty', isCorrect: true },
-            { label: 'Seven', isCorrect: false },
+            { label: 'Un', isCorrect: false },
+            { label: 'Neuf', isCorrect: false },
+            { label: 'Vingt', isCorrect: true },
+            { label: 'Sept', isCorrect: false },
         ],
     },
     {
         type: 'QCM',
-        label: 'Which creature is Gollum originally before being corrupted by the One Ring?',
+        label: "Quelle créature était Gollum à l'origine avant d'être corrompu par l'Anneau Unique ?",
         points: 15,
         choices: [
-            { label: 'Elf', isCorrect: false },
+            { label: 'Elfe', isCorrect: false },
             { label: 'Hobbit', isCorrect: true },
-            { label: 'Dwarf', isCorrect: false },
-            { label: 'Human', isCorrect: false },
+            { label: 'Nain', isCorrect: false },
+            { label: 'Homme', isCorrect: false },
         ],
     },
     {
         type: 'QCM',
-        label: 'What is the name of the wizard who leads the Fellowship of the Ring?',
+        label: "Quel est le nom du magicien qui mène la Communauté de l'Anneau ?",
         points: 10,
         choices: [
-            { label: 'Saruman', isCorrect: false },
+            { label: 'Saroumane', isCorrect: false },
             { label: 'Gandalf', isCorrect: true },
             { label: 'Radagast', isCorrect: false },
             { label: 'Alatar', isCorrect: false },
@@ -102,7 +102,7 @@ export const mockQuestions: QuestionDto[] = [
     },
     {
         type: 'QCM',
-        label: "What is the name of the spider that Frodo battles in Shelob's Lair?",
+        label: "Quel est le nom de l'araignée que Frodon affronte dans la tanière de Shelob ?",
         points: 15,
         choices: [
             { label: 'Ungoliant', isCorrect: false },
