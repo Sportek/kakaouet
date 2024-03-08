5d26db96cb48e48c86586fc8a8951b78d52fd5c8
Ozone <tho.petrie@gmail.com>
2024-03-08 16:26:46 -0500

    100% coverage for chat component

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -10,16 +10,6 @@ export class ChatComponent {
     message: string;
     constructor(public gameService: GameService) {}
 
-    // getGameUser(userId: string): GameUser | null {
-    //     if (this.gameService.game) {
-    //         // _id de mongo de base, accepté par le prof.
-    //         // eslint-disable-next-line no-underscore-dangle
-    //         return this.gameService.game?.users.filter((user) => user._id === userId)[0];
-    //     }
-
-    //     return null;
-    // }
-
     sendMessage() {
         this.gameService.sendMessage(this.message);
         this.message = '';
