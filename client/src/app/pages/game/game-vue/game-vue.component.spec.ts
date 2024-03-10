9ddbdada3ed62f3b1ce2cb87816568197ada7f6e
Ozone <tho.petrie@gmail.com>
2024-03-10 17:47:09 -0400

    100% coverage for game-vue-component

--- client/src/app/pages/game/game-vue/game-vue.component.spec.ts
+++ client/src/app/pages/game/game-vue/game-vue.component.spec.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -9,62 +7,29 @@ import { ActivatedRoute } from '@angular/router';
 import { ChatComponent } from '@app/components/chat/chat.component';
 import { GlobalLayoutComponent } from '@app/components/global-layout/global-layout.component';
 import { HeaderComponent } from '@app/components/header/header.component';
+import { WORKING_QUIZ } from '@app/fake-quizzes';
 import { GameService } from '@app/services/game/game.service';
-import { QuestionType } from '@common/types';
-import { of } from 'rxjs';
+import { Question } from '@common/types';
+import { cloneDeep } from 'lodash';
 import { GameVueComponent } from './game-vue.component';
-
-class MockGameService {
-    question = of({
-        // Mockez un objet question conforme à ce que votre composant attend
-        type: QuestionType.QCM,
-    });
-    cooldown = of(0); // Adaptez la valeur mockée selon vos besoins
-    isFinalAnswer = of(false); // Adaptez selon vos besoins
-    client = of({ score: 0 }); // Adaptez l'objet mocké selon vos besoins
-    answer = of(null); // Adaptez selon vos besoins
-
-    actualQuestion = of({
-        type: 'QCM',
-        label: 'Sample Question',
-        points: 1,
-        choices: [{ label: 'Choice 1', isCorrect: true }],
-    });
-    canChangeChoices = true;
-    selectedChoices = [];
-    gameState = 'SOME_STATE';
-    user = { id: '123', name: 'John Doe', score: 0 };
-
-    init = jasmine.createSpy('init').and.returnValue(Promise.resolve());
-    selectChoice = jasmine.createSpy('selectChoice');
-    getCorrectAnswers = jasmine.createSpy('getCorrectAnswers').and.returnValue(of([0]));
-
-    getQuestionPoints = jasmine.createSpy('getQuestionPoints').and.returnValue(of(5));
-    getQuestionLabel = jasmine.createSpy('getQuestionLabel').and.returnValue(of('Quelle est la question ?'));
-    isQCMQuestion = jasmine.createSpy('isQCMQuestion').and.returnValue(of(true));
-    getQCMChoices = jasmine.createSpy('getQCMChoices').and.returnValue(of([{ label: 'Réponse 1' }, { label: 'Réponse 2' }]));
-
-    timer = { getTimer: jasmine.createSpy('getTimer').and.returnValue(30) };
-    isSelected = jasmine.createSpy('isSelected').and.callFake((index) => index === 0);
-    giveUp = jasmine.createSpy('giveUp');
-}
 
 describe('GameVueComponent', () => {
     let component: GameVueComponent;
     let fixture: ComponentFixture<GameVueComponent>;
+    let gameService: GameService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [GameVueComponent, ChatComponent, GlobalLayoutComponent, HeaderComponent],
             imports: [HttpClientTestingModule, MatIconModule, FormsModule],
             providers: [
-                { provide: GameService, useClass: MockGameService },
                 { provide: MatSnackBar, useValue: { open: jasmine.createSpy('open') } },
                 { provide: ActivatedRoute, useValue: { snapshot: { paramMap: new Map().set('id', 'test') } } },
             ],
         }).compileComponents();
         fixture = TestBed.createComponent(GameVueComponent);
         component = fixture.componentInstance;
+        gameService = TestBed.inject(GameService);
         fixture.detectChanges();
     });
 
@@ -72,58 +37,71 @@ describe('GameVueComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    // it('should initialize game with given id', async () => {
-    //     const gameService = TestBed.inject(GameService);
-    //     await fixture.whenStable();
-    //     expect(gameService.init).toHaveBeenCalledWith('test');
-    // });
+    describe('keyboardChoices()', () => {
+        it('should return immediately upon input event', () => {
+            const inputElement = document.createElement('INPUT');
+            inputElement.setAttribute('type', 'text');
+            document.body.appendChild(inputElement);
+            const event = new KeyboardEvent('keydown', { key: 'Enter' });
+            inputElement.dispatchEvent(event);
 
-    // it('should not process keyboard events when focused on text input', () => {
-    //     const event = new KeyboardEvent('keydown', { key: 'A' });
-    //     Object.defineProperty(event, 'target', { value: { tagName: 'INPUT', getAttribute: () => 'text' } });
-    //     component.keyboardChoices(event);
-    //     const gameService = TestBed.inject(GameService);
-    //     expect(gameService.selectChoice).not.toHaveBeenCalled();
-    // });
+            const selectAnswerComponent = spyOn(component, 'selectAnswer');
 
-    // it('should show snackbar message if no choice is selected', () => {
-    //     const snackBar = TestBed.inject(MatSnackBar);
-    //     component.setResponseAsFinal();
-    //     expect(snackBar.open).toHaveBeenCalledWith('Veuillez sélectionner au moins une réponse', '❌', { duration: 2000 });
-    // });
+            component.keyboardChoices(event);
 
-    // it('should select a choice when key pressed corresponds to a choice', () => {
-    //     const event = new KeyboardEvent('keydown', { key: '1' });
-    //     Object.defineProperty(event, 'target', { value: { tagName: 'BOB', getAttribute: () => 'text' } });
-    //     component.keyboardChoices(event);
-    //     const gameService = TestBed.inject(GameService);
-    //     expect(gameService.selectChoice).toHaveBeenCalledWith(0);
-    // });
+            expect(selectAnswerComponent).not.toHaveBeenCalled();
+        });
 
-    // it('should set canChangeChoices to false after setResponseAsFinal is called', () => {
-    //     component.gameService.canChangeChoices = true;
-    //     component.gameService.selectedChoices.push(1);
-    //     component.setResponseAsFinal();
-    //     expect(component.gameService.canChangeChoices).toBeFalse();
-    // });
+        it('should select answer if correct KeyboardEvent submitted', () => {
+            const inputElement = document.createElement('div');
+            document.body.appendChild(inputElement);
+            const event = new KeyboardEvent('keydown', { key: '1' });
+            inputElement.dispatchEvent(event);
 
-    // it('should call setResponseAsFinal when Enter is pressed and canChangeChoices is true', () => {
-    //     component.gameService.canChangeChoices = true;
-    //     spyOn(component, 'setResponseAsFinal');
-    //     const event = new KeyboardEvent('keydown', { key: 'Enter' });
-    //     Object.defineProperty(event, 'target', { value: { tagName: 'BOB', getAttribute: () => 'text' } });
-    //     component.keyboardChoices(event);
-    //     expect(component.setResponseAsFinal).toHaveBeenCalled();
-    // });
+            const selectAnswerSpy = spyOn(component, 'selectAnswer');
+            component.question = cloneDeep(WORKING_QUIZ.questions[0] as Question);
 
-    // it('should correctly identify if an answer is not included in the correct answers when gameState is DisplayQuestionResults', () => {
-    //     component.gameService.getCorrectAnswers = jasmine.createSpy().and.returnValue(of([1, 2]));
-    //     component.gameService.gameState = GameState.DisplayQuestionResults;
-    //     component.isIncorrectAnswer(0).subscribe((isIncorrect) => {
-    //         expect(isIncorrect).toBeTrue();
-    //     });
-    //     component.isIncorrectAnswer(1).subscribe((isIncorrect) => {
-    //         expect(isIncorrect).toBeFalse();
-    //     });
-    // });
+            component.keyboardChoices(event);
+
+            expect(selectAnswerSpy).toHaveBeenCalledWith(0);
+        });
+
+        it('should set current answer as final', () => {
+            const inputElement = document.createElement('div');
+            document.body.appendChild(inputElement);
+            const event = new KeyboardEvent('keydown', { key: 'Enter' });
+            inputElement.dispatchEvent(event);
+
+            const setResponseAsFinalSpy = spyOn(component, 'setResponseAsFinal');
+            component.question = cloneDeep(WORKING_QUIZ.questions[0] as Question);
+
+            component.keyboardChoices(event);
+
+            expect(setResponseAsFinalSpy).toHaveBeenCalled();
+        });
+    });
+
+    it('should select answer', () => {
+        const gameServiceSpy = spyOn(gameService, 'selectAnswer');
+        component.selectAnswer(0);
+        expect(gameServiceSpy).toHaveBeenCalledWith(0);
+    });
+
+    it('should give up', () => {
+        const gameServiceSpy = spyOn(gameService, 'giveUp');
+        component.giveUp();
+        expect(gameServiceSpy).toHaveBeenCalled();
+    });
+
+    it('should return the answer', () => {
+        component.answer = '1';
+        const result = component.isSelected(1);
+        expect(result).toBeTrue();
+    });
+
+    it('should set response as final', () => {
+        const gameServiceSpy = spyOn(gameService, 'setResponseAsFinal');
+        component.setResponseAsFinal();
+        expect(gameServiceSpy).toHaveBeenCalled();
+    });
 });
