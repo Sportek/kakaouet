fee81dda6a152edf6b7dd70f24a060a8002b573b
Gabriel Landry <gablandry31@gmail.com>
2024-03-17 17:10:11 +0000

    Test/socket guard

--- client/src/app/guards/socket-connection/socket-connection.guard.spec.ts
+++ client/src/app/guards/socket-connection/socket-connection.guard.spec.ts
@@ -1,17 +1,43 @@
-// import { TestBed } from '@angular/core/testing';
-// import { CanActivateFn } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { SocketService } from '@app/services/socket/socket.service';
+import { socketConnectionGuard } from './socket-connection.guard';
 
-// import { socketConnectionGuard } from './socket-connection.guard';
+describe('socketConnectionGuard', () => {
+    let router: jasmine.SpyObj<Router>;
+    let socketService: jasmine.SpyObj<SocketService>;
+    let mockActivatedRouteSnapshot: ActivatedRouteSnapshot;
+    let mockRouterStateSnapshot: jasmine.SpyObj<RouterStateSnapshot>;
 
-// describe('socketConnectionGuard', () => {
-//     const executeGuard: CanActivateFn = async (...guardParameters) =>
-//         // TestBed.runInInjectionContext(async () => socketConnectionGuard(...guardParameters));
+    const executeGuard: CanActivateFn = async (...guardParameters) =>
+        TestBed.runInInjectionContext(async () => {
+            return socketConnectionGuard(...guardParameters) as boolean;
+        });
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//     });
+    beforeEach(() => {
+        router = jasmine.createSpyObj('Router', ['navigate']);
+        socketService = jasmine.createSpyObj('SocketService', [], { isConnected: false });
+        mockActivatedRouteSnapshot = new ActivatedRouteSnapshot();
+        mockRouterStateSnapshot = jasmine.createSpyObj('RouterStateSnapshot', [], { url: '/dummy-url' });
 
-//     it('should be created', () => {
-//         expect(executeGuard).toBeTruthy();
-//     });
-// });
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: Router, useValue: router },
+                { provide: SocketService, useValue: socketService },
+            ],
+        });
+    });
+
+    it('should be created', () => {
+        expect(executeGuard).toBeTruthy();
+    });
+
+    it('should navigate to home if socket is not connected', async () => {
+        socketService.isConnected = false;
+
+        const result = await TestBed.runInInjectionContext(async () => socketConnectionGuard(mockActivatedRouteSnapshot, mockRouterStateSnapshot));
+
+        expect(result).toBeFalse();
+        expect(router.navigate).toHaveBeenCalledWith(['/']);
+    });
+});
