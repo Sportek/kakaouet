dc7b761ad74c6cbd357378f67886459285f8d172
mjamil22 <mohammad-jamil.miah@polymtl.ca>
2024-03-17 21:53:56 +0000

    Testserver2

--- server/app/middlewares/logger/logger.middleware.spec.ts
+++ server/app/middlewares/logger/logger.middleware.spec.ts
@@ -1,16 +1,34 @@
 import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request, Response } from 'express';
 import { LoggerMiddleware } from './logger.middleware';
 
 describe('LoggerMiddleware', () => {
     let middleware: LoggerMiddleware;
+    let mockLogger: Logger;
+    let mockReq: Partial<Request>;
+    let mockRes: Partial<Response>;
+    let mockNext: jest.Mock;
+
     beforeEach(async () => {
+        mockLogger = new Logger();
+        mockReq = {
+            method: 'GET',
+            baseUrl: '/test',
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            signedCookies: { user_token: '123' },
+        };
+        mockRes = {};
+        mockNext = jest.fn();
+
+        jest.spyOn(mockLogger, 'log');
+
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 LoggerMiddleware,
                 {
                     provide: Logger,
-                    useValue: new Logger(),
+                    useValue: mockLogger,
                 },
             ],
         }).compile();
@@ -20,5 +38,12 @@ describe('LoggerMiddleware', () => {
 
     it('should be defined', () => {
         expect(middleware).toBeDefined();
+    });
+
+    it('should log the request method, baseUrl, and user token', async () => {
+        middleware.use(mockReq as Request, mockRes as Response, mockNext);
+
+        expect(mockLogger.log).toHaveBeenCalledWith(`HTTP ${mockReq.method} ${mockReq.baseUrl} (user: ${mockReq.signedCookies['user_token']})`);
+        expect(mockNext).toHaveBeenCalled();
     });
 });
