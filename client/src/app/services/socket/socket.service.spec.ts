572a568f46dd167740fb4e90085d88e1b04fa38e
Gabriel Landry <gablandry31@gmail.com>
2024-03-17 17:09:36 +0000

    Fix Problème à la déconnection dans la file d'attente, certains joueurs ne...

--- client/src/app/services/socket/socket.service.spec.ts
+++ client/src/app/services/socket/socket.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
+import { NotificationService } from '@app/services/notification/notification.service';
 import { SocketService } from './socket.service';
 
 describe('SocketService', () => {
     let service: SocketService;
+    let notificationServiceMocked: jasmine.SpyObj<NotificationService>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        notificationServiceMocked = jasmine.createSpyObj('NotificationService', ['info']);
+        TestBed.configureTestingModule({
+            providers: [{ provide: NotificationService, useValue: notificationServiceMocked }],
+        });
         service = TestBed.inject(SocketService);
     });
 
