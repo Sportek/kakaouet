6c3cc013e8bae255e1b3859815388ceb2a17f524
Ozone <tho.petrie@gmail.com>
2024-04-07 14:45:26 -0400

    Organiser now scores players individually

--- client/src/app/pages/game/organisator/organisator.component.ts
+++ client/src/app/pages/game/organisator/organisator.component.ts
@@ -17,13 +17,15 @@ export class OrganisatorComponent implements OnInit, OnDestroy {
     players: PlayerClient[];
     choices: ChoiceData[];
     timerIsRunning;
+    currentPlayer: PlayerClient;
+    currentPlayerIndex: number;
+    playerRatings: Map<string, number> = new Map<string, number>();
 
     // for QRL
     selectedResponseQRL: string | null = null;
     // for QRL
     playersQRL: string | undefined;
     // for QRL
-    playerRatings: Map<string, number> = new Map<string, number>();
     // for QRL
     interactedCount: number = 0;
     // for QRL
@@ -57,29 +59,8 @@ export class OrganisatorComponent implements OnInit, OnDestroy {
     }
 
     // for QRL
-    getAnswerQRL(): { name: string; answer: string }[] {
-        const responses: { name: string; answer: string }[] = [];
-        if (this.actualQuestion?.question.type === QuestionType.QRL) {
-            for (const player of this.players) {
-                const answer = player.answers?.answer;
-                if (typeof answer === 'string') {
-                    responses.push({ name: player.name, answer });
-                }
-            }
-        }
-        return responses.sort((playerA, playerB) => playerA.name.localeCompare(playerB.name));
-    }
-
-    // for QRL
     getPlayers(): string[] {
-        return this.getAnswerQRL().map((player) => player.name);
-    }
-
-    // for QRL
-    selectPlayer(name: string): void {
-        const player = this.getAnswerQRL().find((p) => p.name === name);
-        this.playersQRL = player?.name;
-        this.selectedResponseQRL = player ? player.answer : null;
+        return this.players.map((player) => player.name);
     }
 
     // for QRL
@@ -100,6 +81,7 @@ export class OrganisatorComponent implements OnInit, OnDestroy {
 
     sendRating(playerName: string) {
         this.gameService.rateAnswerQRL(playerName, this.playerRatings.get(playerName) ?? 0);
+        this.currentPlayer = this.players[++this.currentPlayerIndex];
     }
 
     // for QRL
@@ -169,32 +151,10 @@ export class OrganisatorComponent implements OnInit, OnDestroy {
             this.gameService.players.subscribe((players) => {
                 this.players = players;
                 this.calculateHistogramData();
+                this.currentPlayer = players[0];
+                this.currentPlayerIndex = 0;
             }),
         );
-
-        /* this.gameService.gameState.subscribe((state) => {
-            if (this.actualQuestion?.question.type === QuestionType.QRL && state === GameState.DisplayQuestionResults) {
-                this.choices = [
-                    { text: '0%', amount: 0, isCorrect: true },
-                    { text: '50%', amount: 0, isCorrect: true },
-                    { text: '100%', amount: 0, isCorrect: true },
-                ];
-
-                 this.playerRatings.forEach((value) => {
-                     switch (value) {
-                        case 'zero':
-                            this.choices[0].amount++;
-                            break;
-                        case 'half':
-                            this.choices[1].amount++;
-                            break;
-                        case 'full':
-                            this.choices[2].amount++;
-                            break;
-                    }
-                });
-            });
-        }*/
     }
 
     filterPlayers(): PlayerClient[] {
