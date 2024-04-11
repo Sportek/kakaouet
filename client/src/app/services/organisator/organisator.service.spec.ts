1ab85e70acfa42bc0d30d0dc20f8d9f6ec4d36e6
Ozone <tho.petrie@gmail.com>
2024-04-10 22:48:38 -0400

    Client Test fix

--- client/src/app/services/organisator/organisator.service.spec.ts
+++ client/src/app/services/organisator/organisator.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OrganisatorService } from './organisator.service';
 
 describe('OrganisatorService', () => {
     let service: OrganisatorService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
         service = TestBed.inject(OrganisatorService);
     });
 
