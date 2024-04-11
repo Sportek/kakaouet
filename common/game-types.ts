ef2f4733dcdd1fe27e28afbf64960ee17e1da2fd
Elie Boulanger <elieboulanger33@gmail.com>
2024-04-11 23:48:48 +0000

    Feature/sort users

--- common/game-types.ts
+++ common/game-types.ts
@@ -60,6 +60,18 @@ export enum InteractionStatus {
     abandoned = 'abandoned'
 
 }
+
+export enum SortingCriteria{
+    name = 'name', 
+    score = 'score',
+    status = 'status',
+}
+
+export enum SortOrder{
+    ascending = 'asc',
+    descending = 'desc',
+}
+
 export interface Client { name: string; role: GameRole; score: number };
 export interface GameRestricted { code: string; quizName: string; type: GameType };
 export interface SocketResponse { isSuccess: boolean; message?: string };
