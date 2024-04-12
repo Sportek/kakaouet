f565356344cd38bcbcf0c763c89943b52869ed51
Gabriel Landry <gablandry31@gmail.com>
2024-04-12 19:39:15 +0000

    Fix/styling

---
+++ client/src/app/components/confirmation/confirmation.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmationComponent } from './confirmation.component';
+
+describe('ConfirmationComponent', () => {
+    let component: ConfirmationComponent;
+    let fixture: ComponentFixture<ConfirmationComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ConfirmationComponent],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: {
+                        close: jasmine.createSpy('close'),
+                    },
+                },
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: { message: 'Are you sure?' },
+                },
+            ],
+        });
+        fixture = TestBed.createComponent(ConfirmationComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
