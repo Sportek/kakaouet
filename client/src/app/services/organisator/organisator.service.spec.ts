ed3e40eb73fab72326b91b27a9dae5897d2a2206
Diarse <70977656+NujabesCoder@users.noreply.github.com>
2024-04-13 17:55:06 -0400

    reste plus que rateAnswerQRL et sendRating comme test

--- client/src/app/services/organisator/organisator.service.spec.ts
+++ client/src/app/services/organisator/organisator.service.spec.ts
@@ -3,25 +3,46 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { GameService } from '@app/services/game/game.service';
-import { InteractionStatus, PlayerClient } from '@common/game-types';
+import { Variables } from '@common/enum-variables';
+import { ActualQuestion, InteractionStatus, PlayerClient } from '@common/game-types';
 import { GameRole, QuestionType } from '@common/types';
-import { OrganisatorService } from './organisator.service';
+import { OrganisatorService, baseQRLRatings } from './organisator.service';
 
 describe('OrganisatorService', () => {
     let service: OrganisatorService;
-    let mockSnackbar: jasmine.SpyObj<MatSnackBar>;
+    // let mockSnackbar: jasmine.SpyObj<MatSnackBar>;
     let gameServiceMock: jasmine.SpyObj<GameService>;
 
     beforeEach(() => {
-        gameServiceMock = jasmine.createSpyObj('GameService', ['filterPlayers']);
+        gameServiceMock = jasmine.createSpyObj('GameService', ['filterPlayers', 'recentInteractions']);
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
             providers: [
-                { provide: MatSnackBar, useValue: mockSnackbar },
+                { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
                 { provide: GameService, useValue: gameServiceMock },
             ],
         });
         service = TestBed.inject(OrganisatorService);
+        service.players = [
+            {
+                name: 'Alice',
+                role: GameRole.Player,
+                score: 0,
+                isExcluded: false,
+                hasGiveUp: false,
+                isMuted: false,
+                interactionStatus: InteractionStatus.interacted,
+            },
+            {
+                name: 'Bob',
+                role: GameRole.Player,
+                score: 0,
+                isExcluded: false,
+                hasGiveUp: false,
+                isMuted: false,
+                interactionStatus: InteractionStatus.noInteraction,
+            },
+        ];
     });
 
     it('should be created', () => {
@@ -145,5 +166,162 @@ describe('OrganisatorService', () => {
                 { text: 'fatty tuna', amount: 0, isCorrect: false },
             ]);
         });
+    });
+
+    describe('setActualQuestion', () => {
+        beforeEach(() => {
+            spyOn(service, 'calculateChoices');
+        });
+        it('should set the actualQuestion and update choices if the question type is QRL', () => {
+            const question: ActualQuestion = {
+                question: {
+                    _id: 'q1',
+                    type: QuestionType.QRL,
+                    text: 'Estimate the value of pi to one decimal place.',
+                    points: 10,
+                    createdAt: new Date(),
+                    lastModification: new Date(),
+                },
+                totalQuestion: 5,
+                actualIndex: 1,
+            };
+
+            service.setActualQuestion(question);
+            expect(service.actualQuestion).toEqual(question);
+            expect(service.choices).toEqual(baseQRLRatings);
+            expect(service.calculateChoices).toHaveBeenCalled();
+        });
+
+        it('should set the actualQuestion but not update choices if the question type is not QRL', () => {
+            const question: ActualQuestion = {
+                question: {
+                    _id: 'q2',
+                    type: QuestionType.QCM,
+                    text: 'What is 2 + 2?',
+                    points: 5,
+                    createdAt: new Date(),
+                    lastModification: new Date(),
+                    choices: [],
+                },
+                totalQuestion: 5,
+                actualIndex: 1,
+            };
+
+            service.setActualQuestion(question);
+            expect(service.actualQuestion).toEqual(question);
+            expect(service.choices).not.toEqual(baseQRLRatings);
+            expect(service.calculateChoices).toHaveBeenCalled();
+        });
+    });
+
+    describe('setPlayers', () => {
+        it('should sort, filter players and set the first player as current', () => {
+            const players: PlayerClient[] = [
+                {
+                    name: 'Charlie',
+                    score: 20,
+                    isExcluded: false,
+                    hasGiveUp: false,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+                {
+                    name: 'Alice',
+                    score: 10,
+                    isExcluded: false,
+                    hasGiveUp: false,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+                {
+                    name: 'Bob',
+                    score: 15,
+                    isExcluded: false,
+                    hasGiveUp: true,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+            ];
+
+            spyOn(service, 'filterPlayers').and.returnValue([
+                {
+                    name: 'Alice',
+                    score: 10,
+                    isExcluded: false,
+                    hasGiveUp: false,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+                {
+                    name: 'Charlie',
+                    score: 20,
+                    isExcluded: false,
+                    hasGiveUp: false,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+            ]);
+            spyOn(service, 'calculateChoices');
+            service.setPlayers(players);
+
+            expect(service.players).toEqual([
+                {
+                    name: 'Alice',
+                    score: 10,
+                    isExcluded: false,
+                    hasGiveUp: false,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+                {
+                    name: 'Charlie',
+                    score: 20,
+                    isExcluded: false,
+                    hasGiveUp: false,
+                    role: GameRole.Player,
+                    isMuted: false,
+                    interactionStatus: InteractionStatus.interacted,
+                },
+            ]);
+            expect(service.currentPlayer).toEqual({
+                name: 'Alice',
+                score: 10,
+                isExcluded: false,
+                hasGiveUp: false,
+                role: GameRole.Player,
+                isMuted: false,
+                interactionStatus: InteractionStatus.interacted,
+            });
+            expect(service.currentPlayerIndex).toBe(0);
+            expect(service.calculateChoices).toHaveBeenCalled();
+        });
+    });
+
+    it('histogram should be modified', () => {
+        gameServiceMock.recentInteractions.get = jasmine.createSpy().and.callFake((name) => {
+            const now = 5;
+            return name === 'Alice' ? now - 1 : now + 1;
+        });
+        service.calculateHistogram(Variables.HistrogramCooldown);
+        expect(service.histogram.hasModified).toBe(2);
+        expect(service.histogram.hasNotModified).toBe(0);
+        expect(gameServiceMock.recentInteractions.get).toHaveBeenCalledTimes(2);
+    });
+
+    it('histogram should not be modified', () => {
+        gameServiceMock.recentInteractions.get = jasmine.createSpy().and.callFake((name) => {
+            const now = Date.now();
+            return name === 'Alice' ? now - 1000 : now + 100000;
+        });
+        service.calculateHistogram(Variables.HistrogramCooldown);
+        expect(service.histogram.hasModified).toBe(0);
+        expect(service.histogram.hasNotModified).toBe(2);
+        expect(gameServiceMock.recentInteractions.get).toHaveBeenCalledTimes(2);
     });
 });
