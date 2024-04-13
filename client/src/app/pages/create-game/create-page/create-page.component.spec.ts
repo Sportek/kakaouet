cfcd3ebfcef4bdca5b8383e9e51f7906036ee06b
Elie Boulanger <elie-2.boulanger@polymtl.ca>
2024-04-13 19:49:24 -0400

    Update create-page.component.spec.ts

--- client/src/app/pages/create-game/create-page/create-page.component.spec.ts
+++ client/src/app/pages/create-game/create-page/create-page.component.spec.ts
@@ -1,52 +1,32 @@
-/* import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { QuizService } from '@app/services/quiz/quiz.service';
-import { Quiz } from '@common/types';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { CreatePageComponent } from './create-page.component';
 
 describe('CreatePageComponent', () => {
     let component: CreatePageComponent;
     let fixture: ComponentFixture<CreatePageComponent>;
-    let quizServiceMock: jasmine.SpyObj<QuizService>;
-    let mockQuizzes: Quiz[];
+    let quizServiceMock: any;
+    let matSnackBarMock: any;
 
     beforeEach(async () => {
-        mockQuizzes = [
-            {
-                _id: '123',
-                title: 'Quiz 1',
-                duration: 60,
-                description: 'This is the description of question 1',
-                visibility: true,
-                questions: [],
-                createdAt: new Date(),
-                lastModification: new Date(),
-            },
-            {
-                _id: '456',
-                title: 'Quiz 2',
-                duration: 60,
-                description: 'This is the description of question 2',
-                visibility: true,
-                questions: [],
-                createdAt: new Date(),
-                lastModification: new Date(),
-            },
-        ];
-
-        quizServiceMock = jasmine.createSpyObj('QuizService', ['getAllQuizzes']);
-        quizServiceMock.getAllQuizzes.and.returnValue(of(mockQuizzes));
+        quizServiceMock = {
+            getAllQuizzes: jasmine.createSpy('getAllQuizzes').and.returnValue(of([{ id: '1', name: 'Quiz 1' }])),
+            getRandomQuiz: jasmine.createSpy('getRandomQuiz').and.returnValue(of({ id: 'random', name: 'Random Quiz' }))
+        };
+        matSnackBarMock = {
+            open: jasmine.createSpy('open')
+        };
 
         await TestBed.configureTestingModule({
             declarations: [CreatePageComponent],
             providers: [
                 { provide: QuizService, useValue: quizServiceMock },
-                { provide: ActivatedRoute, useValue: {} },
+                { provide: MatSnackBar, useValue: matSnackBarMock }
             ],
-            schemas: [CUSTOM_ELEMENTS_SCHEMA],
-            imports: [RouterModule],
+            schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
 
         fixture = TestBed.createComponent(CreatePageComponent);
@@ -58,16 +38,17 @@ describe('CreatePageComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should call getQuizzes on init', () => {
+    it('should fetch quizzes and a random quiz on init', () => {
         expect(quizServiceMock.getAllQuizzes).toHaveBeenCalled();
+        expect(quizServiceMock.getRandomQuiz).toHaveBeenCalled();
+        expect(component.games.length).toBe(2);
     });
 
-    it('should set games to quizzes returned by the service', (done) => {
+    it('should handle errors when fetching a random quiz', () => {
+        quizServiceMock.getRandomQuiz.and.returnValue(throwError(() => new Error('Error')));
         component.getQuizzes();
-        fixture.whenStable().then(() => {
-            expect(component.games).toEqual(mockQuizzes);
-            done();
+        expect(matSnackBarMock.open).toHaveBeenCalledWith('Pas assez de question pour générer un random quiz', 'Fermer', {
+            duration: 3000
         });
     });
 });
- */
