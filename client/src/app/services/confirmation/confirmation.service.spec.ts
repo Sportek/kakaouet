8c803c9cd243a2aa13517818de59f1a5117a8bb7
Mahamane Yacine Mahamadou Lawali <97753440+Siiryas@users.noreply.github.com>
2024-04-14 03:43:29 -0400

    ajout des modifications present dans dev

--- client/src/app/services/confirmation/confirmation.service.spec.ts
+++ client/src/app/services/confirmation/confirmation.service.spec.ts
@@ -1,19 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationComponent } from '@app/components/confirmation/confirmation.component';
+import { of } from 'rxjs';
 import { ConfirmationService } from './confirmation.service';
 
 describe('ConfirmationService', () => {
     let service: ConfirmationService;
+    let dialog: MatDialog;
+    const dialogRefSpyObj = jasmine.createSpyObj({ afterClosed: of(true), close: null });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [MatDialogModule],
+            providers: [
+                ConfirmationService,
+                {
+                    provide: MatDialog,
+                    useValue: {
+                        open: jasmine.createSpy('open').and.returnValue(dialogRefSpyObj),
+                    },
+                },
+            ],
         });
         service = TestBed.inject(ConfirmationService);
+        dialog = TestBed.inject(MatDialog);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should open a dialog with the correct parameters', () => {
+        service.confirm('Confirm this action?', () => {
+            return true;
+        });
+        expect(dialog.open).toHaveBeenCalledWith(ConfirmationComponent, {
+            width: '450px',
+            data: { message: 'Confirm this action?' },
+        });
+    });
+
+    it('should execute callback if confirmed', () => {
+        const callback = jasmine.createSpy('callback');
+        service.confirm('Confirm this action?', callback);
+        expect(callback).toHaveBeenCalled();
     });
 });
