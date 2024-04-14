7803ebdf1a0ad01bf6b26ca68721b1bc2b6c33d3
Gabriel Landry <gablandry31@gmail.com>
2024-04-14 15:42:34 -0400

    Fix des erreurs bleus

--- client/src/app/pages/admin/admin-game-history/admin-game-history.component.spec.ts
+++ client/src/app/pages/admin/admin-game-history/admin-game-history.component.spec.ts
@@ -1,5 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BackgroundComponent } from '@app/components/background/background.component';
+import { GlobalLayoutComponent } from '@app/components/global-layout/global-layout.component';
+import { HeaderComponent } from '@app/components/header/header.component';
+import { SelectorComponent } from '@app/components/selector/selector.component';
 import { HistoryService } from '@app/services/history/history.service';
 import { AdminGameHistoryComponent } from './admin-game-history.component';
 
@@ -14,9 +22,10 @@ describe('AdminGameHistoryComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [AdminGameHistoryComponent],
-            imports: [MatDialogModule],
+            declarations: [AdminGameHistoryComponent, GlobalLayoutComponent, BackgroundComponent, SelectorComponent, HeaderComponent],
+            imports: [MatDialogModule, MatMenuModule, HttpClientModule, MatSnackBarModule],
             providers: [{ provide: HistoryService, useClass: MockHistoryService }],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
     });
 
